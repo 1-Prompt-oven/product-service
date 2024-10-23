9918bc3fd875d83499345fe09527eef13140030a
lazarus0320 <astar5327z@gmail.com>
2024-10-23 16:47:32 +0900

    Feat: 하위 카테고리 조회 구현

---
+++ src/main/java/org/example/productservice/category/infrastructure/CategoryRepository.java
@@ -0,0 +1,19 @@
+package org.example.productservice.category.infrastructure;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.example.productservice.category.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+	List<Category> findByParentCategoryUuid(String parentCategoryUuid);
+
+	List<Category> findByParentCategoryUuidIsNull();
+
+	@Query("SELECT c FROM Category c WHERE c.categoryUuid = :categoryUuid")
+	Optional<Category> findByCategoryUuid(String categoryUuid);
+
+}

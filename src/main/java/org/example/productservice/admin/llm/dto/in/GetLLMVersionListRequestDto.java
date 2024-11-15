b487aeeb369b559f5f925cd520c1b1d78d6de31c
lazarus0320 <astar5327z@gmail.com>
2024-11-15 16:42:12 +0900

    Feat: LLMVersion CRUD 구현

---
+++ src/main/java/org/example/productservice/admin/llm/dto/in/GetLLMVersionListRequestDto.java
@@ -0,0 +1,16 @@
+package org.example.productservice.admin.llm.dto.in;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class GetLLMVersionListRequestDto {
+
+	private Long llmVersionId;
+	private String llmVersionName;
+}

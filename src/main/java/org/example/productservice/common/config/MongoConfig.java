edf167a1a2eaf41bf8c255bb2e264f7344f8fc51
lazarus0320 <astar5327z@gmail.com>
2024-11-28 10:41:01 +0900

    Test: cicd test 27

--- src/main/java/org/example/productservice/common/config/MongoConfig.java
+++ src/main/java/org/example/productservice/common/config/MongoConfig.java
@@ -1,18 +1,14 @@
 package org.example.productservice.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 
 @Configuration
-public class MongoConfig extends AbstractMongoClientConfiguration {
-	@Override
-	protected String getDatabaseName() {
-		return "product-service";
-	}
-
-	@Override
-	protected void configureConverters(MongoCustomConversions.MongoConverterConfigurationAdapter adapter) {
-		adapter.registerConverter(new BinaryToStringConverter());
+public class MongoConfig {
+	@Bean
+	public MongoCustomConversions mongoCustomConversions() {
+		return MongoCustomConversions.create(config ->
+			config.registerConverter(new BinaryToStringConverter()));
 	}
 }

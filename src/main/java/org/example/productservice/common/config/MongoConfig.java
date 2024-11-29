89c49a12adc7fdb0aa6599e15d2da9b0f3f98f55
lazarus0320 <astar5327z@gmail.com>
2024-11-29 15:42:01 +0900

    Test: cicd test 37

--- src/main/java/org/example/productservice/common/config/MongoConfig.java
+++ src/main/java/org/example/productservice/common/config/MongoConfig.java
@@ -1,14 +1,14 @@
-// package org.example.productservice.common.config;
-//
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
-//
-// @Configuration
-// public class MongoConfig {
-// 	@Bean
-// 	public MongoCustomConversions mongoCustomConversions() {
-// 		return MongoCustomConversions.create(config ->
-// 			config.registerConverter(new BinaryToStringConverter()));
-// 	}
-// }
+package org.example.productservice.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+@Configuration
+public class MongoConfig {
+	@Bean
+	public MongoCustomConversions mongoCustomConversions() {
+		return MongoCustomConversions.create(config ->
+			config.registerConverter(new BinaryToStringConverter()));
+	}
+}
